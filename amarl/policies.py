2eb9a49066597b08c1f393a030a8ec2140da183b
SwamyDev <b.raml@gmx.at>
2020-05-01 08:13:06 +0200

    Implement linear lstm policy
    The linear lstm policy is simpler then the one which will be used for
    the social dilemma environments. Because it is simpler it is easier to
    check the algorithm for errors, and on the next iteration step I can add
    more complexity with more confidence.

--- amarl/policies.py
+++ amarl/policies.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn
 import numpy as np
 
-from amarl.models import A2CNet, A2CLinearNet
+from amarl.models import A2CNet, A2CLinearNet, A2CLinearLSTMNet
 
 
 class Policy(abc.ABC):
@@ -20,7 +20,7 @@ class RandomPolicy(Policy):
     def __init__(self, action_space):
         self._action_space = action_space
 
-    def compute_actions(self, observations):
+    def compute_actions(self, observations, dones=None):
         return np.array([self._action_space.sample() for _ in range(len(observations))]), None
 
     def learn_on_batch(self, batch):
@@ -39,7 +39,7 @@ class A2CPolicy(Policy):
         self._model = A2CNet(observation_space.shape, action_space.n).to(self._device)
         self._optimizer = torch.optim.RMSprop(self._model.parameters(), lr=1e-4, alpha=0.99, eps=1e-5)
 
-    def compute_actions(self, observations):
+    def compute_actions(self, observations, dones=None):
         acts_dist, vs = self._model(torch.from_numpy(observations).to(self._device))
         a = acts_dist.sample()
         return a, {'vs': vs.squeeze(dim=1), 'act_dists': acts_dist}
@@ -77,6 +77,71 @@ class A2CPolicy(Policy):
         total_loss.backward()
         nn.utils.clip_grad_norm_(self._model.parameters(), self._gradient_clip)
         self._optimizer.step()
+
+
+class A2CLSTMPolicy(Policy):
+    def __init__(self, observation_space, action_space, gamma=0.99, entropy_loss_weight=0.01, value_loss_weight=0.5,
+                 gradient_clip=30, device='cpu'):
+        self._gamma = gamma
+        self._entropy_loss_weight = entropy_loss_weight
+        self._value_loss_weight = value_loss_weight
+        self._gradient_clip = gradient_clip
+        self._device = device
+
+        self._model = A2CLinearLSTMNet(observation_space.shape[0], action_space.n).to(self._device)
+        self._optimizer = torch.optim.RMSprop(self._model.parameters(), lr=1e-3, alpha=0.99, eps=1e-5)
+        self._hidden = self._model.get_initial_state()
+        self._pending_reset = False
+
+    def compute_actions(self, observations, dones=None):
+        if self._pending_reset:
+            self._hidden = self._model.get_initial_state()
+        self._pending_reset = dones[0]
+
+        acts_dist, vs, self._hidden = self._model(torch.from_numpy(observations).to(self._device), self._hidden)
+        a = acts_dist.sample()
+        return a, {'vs': vs.squeeze(dim=1), 'act_dists': acts_dist}
+
+    def learn_on_batch(self, rollout):
+        rewards = rollout['rewards']
+        dones = rollout['dones']
+        vs = rollout['vs']
+
+        is_terminal = dones[-1][0]
+        next_return = torch.zeros(*rewards[0].shape).to(self._device)
+        if not is_terminal:
+            _, next_vs, self._hidden = self._model(torch.from_numpy(rollout['last_obs']).to(self._device), self._hidden)
+            next_return = next_vs.squeeze(dim=1).detach()
+
+        returns = [None] * len(rollout)
+        advantages = [None] * len(rollout)
+        for i in reversed(range(len(rollout))):
+            r = torch.from_numpy(rewards[i]).to(self._device)
+            d = torch.from_numpy(dones[i]).to(torch.uint8).to(self._device)
+            next_return = r + self._gamma * (1 - d) * next_return
+            advantages[i] = next_return - vs[i].detach()
+            returns[i] = next_return.detach()
+
+        adv = torch.cat(advantages)
+        ret = torch.cat(returns)
+        v = torch.cat(vs)
+        actions = rollout['actions']
+        act_dist = rollout['act_dists']
+        log_prob = torch.cat([d.log_prob(a) for a, d in zip(actions, act_dist)], dim=0)
+        entropy = torch.cat([d.entropy() for d in act_dist], dim=0)
+
+        policy_loss = -(log_prob * adv).mean()
+        value_loss = (ret - v).pow(2).mean()
+        entropy_loss = entropy.mean()
+
+        self._optimizer.zero_grad()
+        total_loss = policy_loss - self._entropy_loss_weight * entropy_loss + self._value_loss_weight * value_loss
+        total_loss.backward()
+        nn.utils.clip_grad_norm_(self._model.parameters(), self._gradient_clip)
+        self._optimizer.step()
+
+        self._hidden[0].detach_()
+        self._hidden[1].detach_()
 
 
 class CommunicationPolicy(Policy):
--- tests/test_policies.py
+++ tests/test_policies.py
@@ -3,7 +3,7 @@ import pytest
 import torch
 from gym.spaces import Box, Discrete
 
-from amarl.policies import A2CPolicy
+from amarl.policies import A2CPolicy, A2CLSTMPolicy
 from amarl.workers import RolloutBatch
 
 
@@ -15,24 +15,42 @@ def device():
 
 
 @pytest.fixture
-def observation_space():
+def img_obs_space():
     return Box(0, 1, shape=(4, 84, 84), dtype=np.float32)
 
 
 @pytest.fixture
-def action_space():
+def linear_obs_space():
+    return Box(0, 1, shape=(8,), dtype=np.float32)
+
+
+@pytest.fixture
+def basic_action_space():
     return Discrete(2)
 
 
 @pytest.fixture
-def a2c(observation_space, action_space, device):
-    return A2CPolicy(observation_space, action_space, device=device)
+def a2c(img_obs_space, basic_action_space, device):
+    return A2CPolicy(img_obs_space, basic_action_space, device=device)
 
 
 @pytest.fixture
-def make_observation_batch(observation_space):
+def a2c_lstm(linear_obs_space, basic_action_space, device):
+    return A2CLSTMPolicy(linear_obs_space, basic_action_space, device=device)
+
+
+@pytest.fixture
+def make_img_batch(img_obs_space):
     def factory(size=3):
-        return np.random.rand(size, *observation_space.shape).astype(np.float32)
+        return np.random.rand(size, *img_obs_space.shape).astype(np.float32)
+
+    return factory
+
+
+@pytest.fixture
+def make_linear_batch(linear_obs_space):
+    def factory(size=3):
+        return np.random.rand(size, *linear_obs_space.shape).astype(np.float32)
 
     return factory
 
@@ -42,25 +60,25 @@ def random_seed(seed):
     torch.manual_seed(np.random.randint(int(1e6)))
 
 
-def test_a2c_produces_computes_correct_action(a2c, make_observation_batch, action_space):
-    actions, _ = a2c.compute_actions(make_observation_batch(100))
+def test_a2c_produces_computes_correct_action(a2c, make_img_batch, basic_action_space):
+    actions, _ = a2c.compute_actions(make_img_batch(100))
     actions = actions.cpu().numpy()
     assert actions.shape == (100,)
-    assert actions.min() == 0 and actions.max() == (action_space.n - 1)
+    assert actions.min() == 0 and actions.max() == (basic_action_space.n - 1)
 
 
-def test_a2c_can_be_trained_to_prefer_a_certain_action_when_in_a_certain_state(a2c, make_observation_batch):
+def test_a2c_can_be_trained_to_prefer_a_certain_action_when_in_a_certain_state(a2c, make_img_batch):
     random_seed(42)
     rollout_length = 5
     batch_size = 16
-    obs = make_observation_batch(batch_size)
+    obs = make_img_batch(batch_size)
     dones = [make_sparse_dones(batch_size) for _ in range(rollout_length)]
 
     initial_dist = get_action_probabilities(*a2c.compute_actions(obs))
     assert initial_dist[1] < 0.6
 
     for _ in range(100):
-        a2c.learn_on_batch(make_rollout(a2c, rollout_length, static_obs=obs, static_dones=dones))
+        a2c.learn_on_batch(make_action_one_rewarded_rollout(a2c, rollout_length, static_obs=obs, static_dones=dones))
 
     trained_dist = get_action_probabilities(*a2c.compute_actions(obs))
     assert trained_dist[1] >= 0.9999
@@ -70,7 +88,7 @@ def get_action_probabilities(_, additional):
     return additional['act_dists'].probs.detach().cpu().numpy()[0]
 
 
-def make_rollout(a2c, rollout_length, static_obs, static_dones):
+def make_action_one_rewarded_rollout(a2c, rollout_length, static_obs, static_dones):
     rollout = RolloutBatch()
     for _ in range(rollout_length):
         acts, additional = a2c.compute_actions(static_obs)
@@ -78,14 +96,18 @@ def make_rollout(a2c, rollout_length, static_obs, static_dones):
         elements.update(additional)
         rollout.append(elements)
 
-    rollout.set_element('rewards', reward_certain_action(rollout['actions'], action_to_reward=1))
+    rollout.set_element('rewards', reward_certain_actions(rollout['actions'], action_to_reward=1))
     rollout.set_element('dones', static_dones)
     rollout.set_element('last_obs', static_obs)
     return rollout
 
 
-def reward_certain_action(actions, action_to_reward):
-    return [((a == action_to_reward) * 2 - 1).cpu().numpy() for a in actions]
+def reward_certain_actions(actions, action_to_reward):
+    return [reward_certain_action(a, action_to_reward) for a in actions]
+
+
+def reward_certain_action(a, action_to_reward):
+    return ((a == action_to_reward) * 2 - 1).cpu().numpy()
 
 
 def make_sparse_dones(size):
@@ -95,3 +117,57 @@ def make_sparse_dones(size):
 def action_distribution(actions, index):
     sum_acts_of_index = (actions == index).sum()
     return sum_acts_of_index / len(actions)
+
+
+def test_a2c_lstm_can_be_trained_to_memorize_first_observation_and_condition_an_action_on_it(a2c_lstm,
+                                                                                             make_linear_batch):
+    random_seed(42)
+    rollout = 5
+    batch_size = 1
+    left_obs = make_linear_batch(batch_size)
+    right_obs = make_linear_batch(batch_size)
+
+    initial_dist = get_last_action_dist(a2c_lstm, left_obs, rollout)
+    assert_is_roughly_uniform(initial_dist)
+    initial_dist = get_last_action_dist(a2c_lstm, right_obs, rollout)
+    assert_is_roughly_uniform(initial_dist)
+
+    for _ in range(1000):
+        first_obs, rewarded_a = (left_obs, 0) if np.random.randint(2) == 0 else (right_obs, 1)
+        a2c_lstm.learn_on_batch(
+            make_last_action_reward_conditioned_on_first_obs_rollout(a2c_lstm, first_obs, rewarded_a, rollout))
+
+    trained_dist = get_last_action_dist(a2c_lstm, left_obs, rollout)
+    assert trained_dist[0] >= 0.99
+    trained_dist = get_last_action_dist(a2c_lstm, right_obs, rollout)
+    assert trained_dist[1] >= 0.99
+
+
+def assert_is_roughly_uniform(dist):
+    assert 0.2 < dist[0] < 0.8
+    assert 0.2 < dist[1] < 0.8
+
+
+def get_last_action_dist(policy, obs, rollout):
+    dist = None
+    for i in range(rollout):
+        done = i == rollout - 1
+        dist = get_action_probabilities(*policy.compute_actions(obs, [done]))
+    return dist
+
+
+def make_last_action_reward_conditioned_on_first_obs_rollout(policy, first_obs, rewarded_action, rollout_len):
+    rollout = RolloutBatch()
+    for i in range(rollout_len):
+        obs = first_obs if i == 0 else np.zeros_like(first_obs)
+        done = [i == rollout_len - 1]
+        acts, additional = policy.compute_actions(obs, done)
+        reward = 0.0
+        if done[0]:
+            reward = 1.0 if acts[0] == rewarded_action else -1.0
+        elements = dict(actions=acts, rewards=np.array([reward]), dones=np.array(done))
+        elements.update(additional)
+        rollout.append(elements)
+
+    rollout.set_element('last_obs', np.zeros_like(first_obs))
+    return rollout
